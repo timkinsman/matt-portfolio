0e5825de63512913125d0503bce0b13722e23a59
timkinsman <tkinsm@gmail.com>
2021-04-03 10:58:02 +1030

    clean up studies, client filter fixed

--- src/studies/CityOfPortPhillip.tsx
+++ src/studies/CityOfPortPhillip.tsx
@@ -1,5 +1,7 @@
 import img from "../images/Clients/client-cityofportphillip.svg";
 
+import {CITY_OF_PORT_PHILLIP} from "../colors"
+
 import { Government } from '../tagging/industry'
 import { Strategy, UI___UX } from '../tagging/capabilities'
 import { 
@@ -19,18 +21,14 @@ import {
 } from '../tagging/researchMethods'
 
 export default {
-    about: "A brand new digital ecosystem including multiple websites and customer facing online services.",
-    brief: "The Sweat app is the next evolutionary step from the Sweat with Kayla brand, which solely featured Kayla Itsines as the main trainer and face of the brand. As the brand transitioned to simply just 'Sweat', two new additional trainers were brought on board by the names of Kelsey Wells and Sjana Elise to provide their own expertise and knowledge by introducing their unique training methods and style. By bringing in a post-pregnancy (SELF) and yoga program (BAM) it would allow for a more inclusive product, facilitating many more women around the world increasing its target demographic and reach.",
-    capabilites: [Strategy, UI___UX],
-    color: "#13676F",
-    credits: ["Trainer photography by Jeremy Simons", "Back-end development by PixelForce"],
-    deliverables: ["App UI design (Both iOS and Android platforms)", "Responsive website design", "Email marketing campaigns", "Editing and exporting of 1000+ Workout videos", "Physical products and apparel", "Signage for launch campaign"],
+    color: CITY_OF_PORT_PHILLIP, //color
     img,
+    redirect: "copp",
+    title: "City of Port Phillip",
+
+    //tagging
+    capabilites: [Strategy, UI___UX],
     industry: [Government],
-    information: "The Sweat app can be downloaded from the Apple Store here and the Google Play Store here The Sweat website can be viewed here The Sweat Facebook page can be viewed here",
-    objective: "I played the principal role of successfully leading the design team whilst forming strong relations with the engineer developers to collaboratively produce the product. Very tight deadlines were needed to be met in an lean working environment, to allow for quick yet high quality succession of results. The general style would change significantly given the rebrand as we moved from one singular trainer to a more encompassing, marketable brand which would house numerous trainers and workout types. This style is evidently more cleaner and minimalistic, allowing it to be characterised by the trainers and the personality of their training styles.",
-    recognitions: ["2016 Apple Watch App of the Year"],
-    redirect: "cityofportphillip",
     researchMethods: [
         Artefact_Review,
         Concept_Usability_Testing,
@@ -46,10 +44,29 @@ export default {
         Stakeholder_Interviews,
         User_Interviews
     ],    
+
+    //study
+    about: ["A brand new digital ecosystem including multiple websites and customer facing online services."],
+    aboutImg: [],
+    brief: ["Design and implement a digital experience platform as part of the Council’s Customer Experience Program."],
+    breifImg: [],
+    background: [
+        "In 2019 Port Phillip Council’s digital heart was in need of a major upgrade. 53% of customers reported that they were unable to find what they were searching for, or that they required further information following their visit to the website.",
+        "At the same time, legacy systems and platforms were impacting efficiency, meaning Council's digital ecosystem required much more than a new lick of paint.",
+        "“The current, clunky system is well and truly past its use-by date. The ageing technology is not meeting the growing demand and expectations from people accessing our websites for everything from requesting services to applying for permits.” - Mayor Gross, City of Port Phillip",
+        "VERSA was awarded a ten year contract to support Port Phillip's digital transformation, leading the redesign and development of a new technical ecosystem.",
+    ],
+    backgroundImg: [],
+    challenge: ["Establish a centralised digital platform for service delivery and engagement that will support the city’s evolution towards a digital-first future."],
+    challengeImg: [],
+    credits: ["Trainer photography by Jeremy Simons", "Back-end development by PixelForce"],
+    deliverables: ["App UI design (Both iOS and Android platforms)", "Responsive website design", "Email marketing campaigns", "Editing and exporting of 1000+ Workout videos", "Physical products and apparel", "Signage for launch campaign"],
+    information: "The Sweat app can be downloaded from the Apple Store here and the Google Play Store here The Sweat website can be viewed here The Sweat Facebook page can be viewed here",
+    objective: "I played the principal role of successfully leading the design team whilst forming strong relations with the engineer developers to collaboratively produce the product. Very tight deadlines were needed to be met in an lean working environment, to allow for quick yet high quality succession of results. The general style would change significantly given the rebrand as we moved from one singular trainer to a more encompassing, marketable brand which would house numerous trainers and workout types. This style is evidently more cleaner and minimalistic, allowing it to be characterised by the trainers and the personality of their training styles.",
+    recognitions: ["2016 Apple Watch App of the Year"],
     results: "I played the principal role of successfully leading the design team whilst forming strong relations with the engineer developers to collaboratively produce the product. Very tight deadlines were needed to be met in an lean working environment, to allow for quick yet high quality succession of results. The general style would change significantly given the rebrand as we moved from one singular trainer to a more encompassing, marketable brand which would house numerous trainers and workout types. This style is evidently more cleaner and minimalistic, allowing it to be characterised by the trainers and the personality of their training styles.",
     shortSummary: "A 10 year contract supporting the development of the City's future core web framework, a custom pattern library supporting 14 sites.",
     slogan: "For the everyday Member, BankVic's existing website was confusing and complex.",
     summary: "The City of Port Phillip is an industry leader in both the technology and fitness sectors. Owner of the #1 ranked grossing health and fitness App in the world, Sweat connects women globally by providing them with the tools they need to shape their best and healthiest life.",
-    title: "City of Port Phillip",
     whatILearnt: "I played the principal role of successfully leading the design team whilst forming strong relations with the engineer developers to collaboratively produce the product. Very tight deadlines were needed to be met in an lean working environment, to allow for quick yet high quality succession of results."
 }
