8ec5afb8b624bb2448fb645b283b90b4c6823f08
timkinsman <tkinsm@gmail.com>
2021-05-02 18:30:45 +0930

    spotify currently playing

--- src/components/layout/Spotify/Spotify.tsx
+++ src/components/layout/Spotify/Spotify.tsx
@@ -3,30 +3,31 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpotify } from "@fortawesome/free-brands-svg-icons";
 import styles from "./Spotify.module.css";
 
-const userName=""
-const apiKey=""
-
 function Spotify() {
-  const [lfmData, updateLfmData] = useState<any>({});
+  const [lfmName, updateLfmName] = useState("Blush");
+  const [lfmText, updateLfmText] = useState("Vynehall");
 
   useEffect(() => {
-    fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=${userName}&api_key=${apiKey}&limit=1&nowplaying=true&format=json`)
+    fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=${process.env.USERNAME}&api_key=${process.env.API_KEY}&limit=1&nowplaying=true&format=json`)
       .then(response => {
         if (response.ok) {
           return response.json();
         }
         throw new Error('error');
       })
-      .then(data => {console.log(data); updateLfmData(data.recenttracks.track[0])})
+      .then(data => {
+        updateLfmName(data.recenttracks.track[0].name)
+        updateLfmText(data.recenttracks.track[0].artist["#text"])
+      })
       .catch(() =>
-        updateLfmData({ error: 'Whoops! Something went wrong with Last.fm' })
+        console.log("error: 'Whoops! Something went wrong with Last.fm'")
       );
   }, []);
 
   return (
     <div className={styles["spotify-container"]}>
         <FontAwesomeIcon icon={faSpotify} className={styles["spotify-icon"]} />
-        <h3 className={styles["spotify-text"]}>Currently listening to <a className="global-border-thin">{lfmData.name}, {lfmData.artist["#text"]}</a></h3>
+        <h3 className={styles["spotify-text"]}>Currently listening to <a className="global-border-thin">{lfmName}, {lfmText}</a></h3>
     </div>
   );
 }
